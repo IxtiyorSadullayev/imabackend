d70f3946d34dac193ed4c33480bc1eb30da7e442
IxtiyorSadullayev <sadullayevixtiyor2200@gmail.com>
2026-01-24 20:51:51 +0500

    DTO ga class-validator qo'shildi

--- src/users/dto/create-classname.dto.ts
+++ src/users/dto/create-classname.dto.ts
@@ -1,9 +1,12 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsString } from "class-validator";
 
 export class CreateClassNameDto{
     @ApiProperty()
+    @IsString()
     readonly classname: string;
 
     @ApiProperty()
+    @IsString()
     readonly description: string;
 }
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,22 +1,29 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsDateString, IsPhoneNumber, IsString } from "class-validator";
 
 export class CreateUserDto {
     @ApiProperty()
+    @IsString()
     readonly login: string;
 
     @ApiProperty()
+    @IsString()
     readonly password: string;
 
     @ApiProperty()
+    @IsString()
     readonly fullname: string;
 
     @ApiProperty()
+    @IsDateString()
     readonly birthday: string;
 
     @ApiProperty()
+    @IsString()
     readonly pas_seria: string;
 
     @ApiProperty()
+    @IsString()
     readonly pas_number: string;
     
     @ApiProperty({ type: "string", title: 'User qanday role ga ega ekanligi aniqlanadi.' })
@@ -26,5 +33,7 @@ export class CreateUserDto {
     readonly className: string;
 
     @ApiProperty()
+    @IsPhoneNumber("UZ")
     readonly phoneNumber: string;
 } 
+    
